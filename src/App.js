328e20bcc13d50115bcd7ab33a029df99ff724d3
José Manuel Molina <jose.molina.develop@gmail.com>
2024-05-13 17:52:42 +0200

    Create project
    Create project and first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./pages/Home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Clients from "./pages/Clients/Clients.js";
+import Projects from "./pages/Projects/Projects.js";
+import Navbar from "./components/navbar/Navbar.js";
+import Footer from "./components/footer/Footer.js";
+import Menu from "./components/menu/Menu.js";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const Layout = () =>{
+    return (
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/projects",
+          element: <Projects />,
+        },
+        {
+          path: "/clients",
+          element: <Clients />,
+        },
+      ]
+    },
+  ]);
+
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
---
+++ src/pages/Clients/Clients.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './clients.scss'
+
+const Clients = () => {
+  return (
+    <div className='clients'>Clients</div>
+  )
+}
+
+export default Clients
